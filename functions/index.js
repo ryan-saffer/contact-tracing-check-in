6c8dcff0494e675bff08e72c65cc1336c7ca616d
Ryan Saffer <ryansaffer@gmail.com>
2020-11-28 19:00:15 +1100

    Added daily removal of entires older than 28 days

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
+const db = admin.database()
 const mailchimp = require('@mailchimp/mailchimp_marketing');
 
 const API_KEY = "8dad51563d4f3fe08062c6dcb02300cf-us12"
@@ -8,6 +11,10 @@ mailchimp.setConfig({
     apiKey: API_KEY,
     server: "us12"
 })
+
+function toTwoDigits(input) {
+    return ("0" + input).slice(-2)
+}
 
 exports.writeToMailchimp = functions.region('australia-southeast1').https.onCall((data, context) => {
 
@@ -24,3 +31,17 @@ exports.writeToMailchimp = functions.region('australia-southeast1').https.onCall
     .then(() => console.log("Write succeeded!"))
     .catch(err => console.error(err))
 });
+
+exports.deleteOutdatedEntries = functions
+    .region('australia-southeast1')
+    .pubsub.schedule('0 0 * * *')
+    .timeZone('Australia/Victoria')
+    .onRun((context) => {
+
+        const date = new Date()
+        date.setDate(date.getDate() - 29) // retain data for 28 days, so delete the data from 29 days ago
+        const dateString = `${date.getFullYear()}${toTwoDigits(date.getMonth() + 1)}${toTwoDigits(date.getDate())}`
+
+        functions.logger.log("Removing entries on date:", dateString)
+        db.ref(dateString).remove()
+    })
